4decc08e1ecd6f76bbe9a9666761c60fbacd498d
Bohdan Vey <bogdan.vey2002@ucu.edu.ua>
2021-05-07 09:49:11 +0300

    first commit

---
+++ src/views/Header.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import './App.css';
+function Header() {
+    return (
+            <div className="App-header">
+                <header>
+
+                </header>
+            </div>
+    );
+}
+
+export default Header
